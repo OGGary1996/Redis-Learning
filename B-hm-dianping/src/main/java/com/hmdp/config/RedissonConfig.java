6ac3654cd1b019e9941290b642ceee4e6f5e7e32
OGGary1996 <zhangke918525@gmail.com>
2025-11-26 18:31:48 -0600

    applicaiton.yaml update

--- B-hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
+++ B-hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
@@ -3,6 +3,7 @@ package com.hmdp.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,12 +14,28 @@ import java.io.IOException;
 * */
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private Integer port;
+    @Value("${spring.data.redis.database}")
+    private Integer database;
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public RedissonClient redissonClient() throws IOException {
-        // 读取默认的redisson配置文件redisson.yaml
-        Config config = Config.fromYAML(
-                RedissonConfig.class.getClassLoader().getResource("redisson.yaml")
-        );
+        Config config = new Config();
+        String redisAddress = "redis://" + host + ":" + port;
+        config.useSingleServer()
+                .setAddress(redisAddress)
+                .setDatabase(database)
+                .setPassword(password)
+                .setConnectionMinimumIdleSize(1)
+                .setConnectionPoolSize(10);
+        config.setThreads(4);
+        config.setNettyThreads(4);
+
         return Redisson.create(config);
     }
 }
